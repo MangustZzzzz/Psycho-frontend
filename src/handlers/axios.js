99f4d8907eb8a9ce24e71839ae844469979cacb2
MangustZzzzz <48924982+MangustZzzzz@users.noreply.github.com>
2023-06-28 22:31:56 +0300

    Update axios.js

--- src/handlers/axios.js
+++ src/handlers/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://server-psychology-master-7a980fe5d8d2.herokuapp.com/" /*|| "http://localhost:3333/"*/,
+  baseURL: "psycho-frontend-dvj541he4-mangustzzzzz.vercel.app" /*|| "http://localhost:3333/"*/,
 });
 
 instance.interceptors.request.use((config) => {
